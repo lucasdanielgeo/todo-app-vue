02c4edd67640520de483a5944929e09dd9dd4cd8
Lucas Daniel <lucasdaniel.geo@gmail.com>
2022-01-22 12:21:45 -0300

    styles: format with prettier

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,76 @@
 <template>
   <div id="app">
-    <TodoApp msg="Todo Vue.js App"/>
+    <TodoApp msg="Todo Vue.js App" />
   </div>
 </template>
 
 <script>
-import TodoApp from './components/TodoApp.vue'
+import TodoApp from "./components/TodoApp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    TodoApp
-  }
-}
+    TodoApp,
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Archivo:wght@100;300;400;500;600;700&family=Open+Sans:wght@300;400;500;600&family=Roboto:wght@500;600;700&display=swap");
 
-  @import url('https://fonts.googleapis.com/css2?family=Archivo:wght@100;300;400;500&family=Open+Sans:wght@300;400;500;600&family=Roboto:wght@500&display=swap');
+*,
+*:after,
+*:before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style-type: none;
+}
 
-  *, 
-  *:after,
-  *:before {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      text-decoration: none;
-      list-style-type: none;
+@media (max-width: 600px) {
+  * {
+    width: 90vw;
   }
+}
 
-  @media (max-width: 600px) {
-    * {
-      width: 90vw;
-    }
-  }
+html,
+body {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-  html, body{
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  background-color: #f2f1f1;
 
-    font-size:62.5%;
-    font-family: 'Open Sans', Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-  }
+  font-size: 62.5%;
+  font-family: "Open Sans", Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
 
+#app {
+  font-size: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 
-  #app {
-    font-size: 12px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
+input,
+textarea,
+select {
+  height: 3.4rem;
+  border: 1.5px rgb(173, 173, 173) solid;
+  border-radius: 5px;
+  padding-left: 15px;
 
-  input, textarea, select{
-    height: 3.4rem;
-    border: 1.5px rgb(173, 173, 173) solid;
-    border-radius: 5px;
-    padding-left: 15px;
-
-    font-family: 'Open Sans', Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-  }
-  input:focus, textarea:focus, select:focus{
-    outline: none;
-  }
+  font-family: "Open Sans", Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+input:focus,
+textarea:focus,
+select:focus {
+  outline: none;
+}
 </style>
