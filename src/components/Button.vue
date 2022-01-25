aa59b0cc876cbd36b92054fe19f20512ff3b0eca
Lucas Daniel <lucasdaniel.geo@gmail.com>
2022-01-25 19:05:25 -0300

    feat: emmit event and receive button props

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,22 +1,28 @@
 <template>
-  <button @click="$emit('clickEvent')" :style="`min-width: ${size}px`">
+  <button
+    @click="$emit('click-event')"
+    :style="`min-width: ${size}px`"
+    :disabled="disabled"
+  >
     {{ name }}
+    <slot></slot>
   </button>
 </template>
 
 <script>
 export default {
-  name: "Button",
-  emits: ["click"],
+  name: 'Button',
+  emits: ['click-event'],
   props: {
     name: String,
     buttonColor: String,
+    disabled: Boolean,
     size: {
       type: Number,
       default: 50,
     },
   },
-};
+}
 </script>
 
 <style>
@@ -30,5 +36,16 @@ button {
   background-color: #e0e0e0;
   color: rgb(75, 94, 95);
   font-weight: 600;
+  display: grid;
+  place-items: center;
+  transition: opacity ease-in-out 200ms, transform ease-in-out 200ms;
+}
+
+button:disabled {
+  opacity: 0.5;
+  background-color: darkcyan;
+}
+button:hover {
+  transform: scale(1.025);
 }
 </style>
