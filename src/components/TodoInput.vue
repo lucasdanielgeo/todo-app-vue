ee5b412af862a676c6773c5dddce5607b792d034
Lucas Daniel <lucasdaniel.geo@gmail.com>
2022-01-25 19:08:01 -0300

    feat: add description textarea

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,50 +1,68 @@
 <template>
   <form class="input" @submit.prevent="handleSubmit">
-    <input type="text" v-model="description" />
-    <Button
-      name="+ Add Todo"
-      :size="105"
-      style="background-color: #00ab66; color: white"
-    />
+    <label for="title">Title</label> <input type="text" v-model="title" />
+    <label for="description">Description</label>
+    <textarea rows="4" v-model="description" />
+    <Button name="+ Add Todo" :size="105" :style="styleButton" />
   </form>
 </template>
 
 <script>
-import Button from "./Button.vue";
+import Button from './Button.vue'
 export default {
   components: {
     Button,
   },
-  name: "TodoInput",
-  emits: ["save"],
+  name: 'TodoInput',
+  emits: ['save'],
   data() {
     return {
-      description: "",
-    };
+      styleButton: {
+        backgroundColor: '#00ab66',
+        color: 'white',
+      },
+      title: '',
+      description: '',
+    }
   },
   methods: {
     handleSubmit() {
-      this.description && this.$emit("save", this.description);
-      this.description = "";
+      this.title &&
+        this.description &&
+        this.$emit('save', this.title, this.description)
+      this.title = ''
+      this.description = ''
     },
   },
-};
+}
 </script>
 
 <style scoped>
 form {
   display: flex;
+  flex-direction: column;
   width: 100%;
   margin-bottom: 0.5rem;
 }
 
 form.input {
-  display: flex;
-  gap: 0.5rem;
+  gap: 1rem;
   width: 100%;
+}
+
+textarea {
+  padding: 10px;
+  resize: none;
+  height: 74px;
+}
+
+label {
+  font-weight: 600;
+  font-size: 1.6rem;
 }
 
 input {
   flex: 1;
+  min-height: 34px;
 }
 </style>
