446b35a785d845501d83ed1273fb4a3cf0eab244
Lucas Daniel <lucasdaniel.geo@gmail.com>
2022-01-29 15:26:34 -0300

    feat: add modal scope

---
+++ src/components/TodoModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="modal-background">
+    <div class="modal-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoModal',
+  props: ['isModalOpen'],
+}
+</script>
+
+<style>
+.modal-background {
+  background-color: rgb(255, 254, 253);
+  width: 350px;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.329);
+  position: fixed;
+  display: none;
+  place-items: center;
+}
+
+.modal-content {
+  width: 100%;
+}
+</style>
