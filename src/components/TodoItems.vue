5183b140560ff0902ee4947b3518d743aa2be1e9
Lucas Daniel <lucasdaniel.geo@gmail.com>
2022-01-29 15:27:27 -0300

    feat: add TodoEdit component

--- src/components/TodoItems.vue
+++ src/components/TodoItems.vue
@@ -1,12 +1,42 @@
 <template>
   <ul>
-    <h2>Todo List 😉</h2>
-    <li v-for="item in reverseItems" :key="item.id">
-      <input
+    <li v-for="item in onlyUncompleted" :key="item.id">
+      <span
+        @dblclick="$emit('edit-todo', item)"
         :class="{ done: item.completed }"
         class="todo-item"
-        :value="item.title"
-      />
+      >
+        <div class="todo-title">{{ item.title }}:</div>
+        <div class="todo-created-at">{{ item.created_at }}</div>
+      </span>
+      <Button
+        @click-event="$emit('complete-todo', item)"
+        :disabled="item.loading"
+        class="button-icon button-icon__check"
+      >
+        <div class="button-icon button-icon__check">
+          <refresh-icon v-if="item.loading" class="button-icon__spin" />
+          <check-icon v-else />
+        </div>
+      </Button>
+      <Button
+        @click-event="$emit('delete-todo', item)"
+        class="button-icon button-icon__delete"
+      >
+        <div class="button-icon button-icon__delete">
+          <trash-icon />
+        </div>
+      </Button>
+    </li>
+    <li v-for="item in onlyCompleted" :key="item.id">
+      <span
+        @dblclick="$emit('edit-todo', item)"
+        :class="{ done: item.completed }"
+        class="todo-item"
+      >
+        <div class="todo-title">{{ item.title }}</div>
+        <div class="todo-created-at">{{ item.created_at }}</div>
+      </span>
       <Button
         @click-event="$emit('complete-todo', item)"
         :disabled="item.loading"
@@ -41,10 +71,16 @@ export default {
   },
   name: 'TodoItem',
   props: ['items'],
-  emits: ['complete-todo', 'delete-todo'],
+  emits: ['complete-todo', 'delete-todo', 'edit-todo'],
   computed: {
     reverseItems() {
-      return this.items.slice(0).reverse()
+      return this.items.slice(0).reverse
+    },
+    onlyCompleted() {
+      return this.items.slice(0).filter((item) => item.completed)
+    },
+    onlyUncompleted() {
+      return this.items.slice(0).filter((item) => !item.completed)
     },
   },
 }
@@ -71,23 +107,32 @@ li {
   gap: 0.5rem;
 }
 
-input.done {
+span.done {
   -webkit-text-decoration-line: line-through; /* Safari */
   text-decoration-line: line-through;
 }
-input.todo-item {
+span.todo-item {
   border: none;
   background-color: rgb(236, 236, 236);
 }
 
-input {
+span {
+  display: flex;
+  justify-content: space-between;
   outline: none;
   height: 34px;
   width: 100%;
   border-radius: 5px;
   display: flex;
   padding-left: 15px;
+  padding-right: 15px;
   align-items: center;
+}
+.todo-title {
+  font-weight: 600;
+}
+.todo-created-at {
+  font-size: 0.9rem;
 }
 
 .button-icon {
